f85b90505642c08719e77904e105454a5157ad79
Yuyu1115 <105599462+Yuyu-1115@users.noreply.github.com>
2025-12-07 00:34:03 +0800

    feat: .ppm downloading capabilities

---
+++ src/HttpUtil.asm
@@ -0,0 +1,26 @@
+.386
+.model flat, stdcall
+
+INCLUDE HttpUtil.inc
+
+.data
+    LINE_BREAK EQU 0Dh, 0Ah
+    htmlResp  BYTE "HTTP/1.1 200 OK", LINE_BREAK
+        BYTE "Content-Type: text/html", LINE_BREAK
+        BYTE "Connection: close", LINE_BREAK
+        BYTE LINE_BREAK
+        BYTE "<h1>Hello from Assembly!</h1><p>This is a test response</p>", 0
+        BYTE 0
+
+; The header of .ppm response
+    ppmHeader BYTE "HTTP/1.1 200 OK", LINE_BREAK
+        BYTE "Content-Disposition: attachment; filename=""asmprite-download.ppm""", LINE_BREAK
+        BYTE "Content-Type: image/x-portable-pixmap", LINE_BREAK
+        BYTE "Connection: close", LINE_BREAK
+        BYTE 0
+
+
+    lengthFmt BYTE "Content-Length: %d", LINE_BREAK
+        BYTE LINE_BREAK, 0
+
+END
--- src/main.asm
+++ src/main.asm
@@ -1,23 +1,84 @@
-.386
-
 INCLUDE Irvine32.inc
+
 INCLUDE WinWrapper.inc  
+INCLUDE WinSocks.inc  
+INCLUDE HttpUtil.inc
 
 .data
-	string DW 06E2Ch, 08A66h
-    buffer HANDLE ?
-    len DWORD 2
-    charsWritten DWORD ?
+  serviceData WSADATA <>
+  hSocket SOCKET_HANDLE ?
+  hClient SOCKET_HANDLE ?
+  service SOCKADDR_IN <>
+  clientAddress SOCKADDR_IN <>
+  clientLength DWORD SIZEOF service
+  buffer BYTE 2048 DUP(?)
+  respBuffer BYTE 64 DUP(?)
+  acceptMsg BYTE "Connection accepted...", 0
+  ppmBody   BYTE "P3", 13, 10
+            BYTE "3 2", 13, 10
+            BYTE "255", 13, 10
+            BYTE "255 0 0", 13, 10
+            BYTE "0 255 0", 13, 10
+            BYTE "0 0 255", 13, 10
+            BYTE "255 255 0", 13, 10
+            BYTE "255 255 255", 13, 10
+            BYTE "0 0 0", 13, 10
+            BYTE 0
 .code
 main PROC
-    invoke CreateConsoleScreenBuffer, (GENERIC_READ OR GENERIC_WRITE), (FILE_SHARE_READ OR FILE_SHARE_WRITE), NULL, CONSOLE_TEXTMODE_BUFFER, NULL
-    mov buffer, eax
+  ; initializa windows sockets with version 2.2
+  invoke WSAStartup, 0202h, ADDR serviceData
+  ; initiate sockaddr_in
+  mov service.sin_family, AF_INET; ipv4
+  mov service.sin_addr, 0; 0.0.0.0
+  invoke htons, 8080;
+  mov service.sin_port, ax;
+  jnz exitProgram
+  ; create TCP socket in IPv4
+  invoke socket, AF_INET, SOCK_STREAM, 0
+  mov hSocket, eax
+  ; bind the socket into local addresses
+  invoke bind, hSocket, ADDR service, SIZEOF service
+  jnz ExitProgram
+  ; start listening
+  invoke listen, hSocket, SOMAXCONN
+  jnz ExitProgram
+ServerLoop:
+  ; establish connection
+  invoke accept, hSocket, ADDR clientAddress, ADDR clientLength
+  mov hClient, eax;
 
-    invoke WriteConsoleW, buffer, ADDR string, len, ADDR charsWritten, NULL
+  mov edx, OFFSET acceptMsg
+  call WriteString
+  call Crlf
 
-    invoke SetConsoleActiveScreenBuffer, buffer
-    
-    call WaitMsg
-    exit
+  invoke recv, hClient, ADDR buffer, LENGTH buffer, 0
+
+  ; sending response
+
+  mov edx, OFFSET ppmHeader
+  call WriteString
+
+  mov edx, OFFSET respBuffer
+  call WriteString
+
+  mov edx, OFFSET ppmBody
+  call WriteString
+
+
+  ; wsprintf return ohow many bytes are written
+  invoke Str_length, OFFSET ppmBody
+  ; preserve length
+  mov ebx, eax
+  invoke Str_length, OFFSET ppmHeader
+  invoke send, hClient, OFFSET ppmHeader, eax, 0
+  invoke wsprintf, OFFSET respBuffer, OFFSET lengthFmt, ebx
+  invoke send, hClient, OFFSET respBuffer, eax, 0
+  invoke send, hClient, OFFSET ppmBody, ebx, 0
+
+  invoke closesocket, hClient
+  jmp ServerLoop
+exitProgram:
+  exit
 main ENDP
 END main
